17bfbc9d83e854830edbcbdf7e71bf80a994c60e
SergioRC70 <sergioromeroc@gmail.com>
2021-12-30 21:10:27 +0100

    Update

--- renderStations.js
+++ renderStations.js
@@ -146,8 +146,9 @@ function renderStations(fecha)
 	$("#meteo").empty();
 	$("#meteo").append("<b>Datos meteorol&oacute;gicos</b><br/><br/>");
 	d3.dsv(";", file_name).then(function(data) {
+		var myMonth = ("0" + (fecha.getMonth() + 1)).slice(-2);
+
 		var filteredData = data.filter(function(row, i) {
-			var myMonth = ("0" + (fecha.getMonth() + 1)).slice(-2);
 			return row.MES == myMonth && row.ESTACION == '102' && (row.MAGNITUD == '81' || row.MAGNITUD == '83' || row.MAGNITUD == '89');
 		});
 
@@ -164,6 +165,75 @@ function renderStations(fecha)
 				$("#meteo").append("Precipitación: " + valor + "<br/>");
 		});
 
+		renderTempMonth();
+
 		$("#meteo").slideDown(1000);
 	});
+}
+
+
+function renderTempMonth() {
+	// set the dimensions and margins of the graph
+	const margin = {top: 10, right: 40, bottom: 90, left: 20},
+	    width = 340 - margin.left - margin.right,
+	    height = 250 - margin.top - margin.bottom;
+
+	// append the svg object to the body of the page
+	const svg = d3.select("#meteo")
+	  .append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+	// Parse the Data
+	//d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv").then( function(data) {
+	d3.dsv(";", "./data/meteo21.csv").then(function(data) {
+
+	var filteredData = data.filter(function(row, i) {
+		return row.MES == '07' && row.ESTACION == '102' && row.MAGNITUD == '83';
+	});
+
+	var nest = [];
+	var keys = ['D01','D02','D03','D04','D05','D06','D07','D08','D09','D10','D11','D11','D12','D13','D14','D15','D16','D17','D18','D19','D20','D21','D22','D23','D24','D25','D26','D27','D28','D29','D30','D31'];
+	keys.forEach(function (item) {
+		var valor = filteredData[0][item];
+		var obj = {
+			key: item,
+			value: valor
+		}
+		nest.push(obj);
+	})
+
+
+	var x = d3.scaleBand()
+	  .range([ 0, width ])
+	  .domain(nest.map(function(d) { return d.key; }))
+	  .padding(0.1);
+	svg.append("g")
+	  .attr("transform", "translate(0," + height + ")")
+	  .call(d3.axisBottom(x).tickFormat(""))
+	  .selectAll("text")
+	    //.attr("transform", "translate(-10,0)rotate(-45)")
+	    .style("text-anchor", "end");
+
+	// Add Y axis
+	var y = d3.scaleLinear()
+	  .domain([0, 35])
+	  .range([ height, 0]);
+	svg.append("g")
+	  .call(d3.axisLeft(y));
+
+	// Bars
+	svg.selectAll("mybar")
+	  .data(nest)
+	  .enter()
+	  .append("rect")
+	    .attr("x", function(d) { return x(d.key); })
+	    .attr("y", function(d) { return y(d.value); })
+	    .attr("width", x.bandwidth())
+	    .attr("height", function(d) { return height - y(d.value); })
+	    .attr("fill", "#69b3a2")
+
+	})
 }
