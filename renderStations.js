4625dee3332bf5c2b5c52db17ff8795095347929
SergioRC70 <sergioromeroc@gmail.com>
2022-01-06 22:11:55 +0100

    Update renderStations.js

--- renderStations.js
+++ renderStations.js
@@ -182,7 +182,7 @@ function renderTempMonth(mes) {
 	$("#meteo").append("<br/><br/><b>Temperatura y precipiationes a lo largo del mes:</b><br/><br/>");
 
 	// set the dimensions and margins of the graph
-	const margin = {top: 10, right: 60, bottom: 20, left: 20},
+	const margin = {top: 10, right: 60, bottom: 40, left: 20},
 	    width = 340 - margin.left - margin.right,
 	    height = 250 - margin.top - margin.bottom;
 
